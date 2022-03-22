345d34c568f529cdefac79faa747c273ce639355
lindabettini <linda.bettini@hotmail.it>
2022-03-22 19:20:19 +0100

    add show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,7 +6,7 @@
   @foreach ($comics as $comic)
   <dic class="col-5 p-3 m-3">
     <a href="{{route('comics.show', $comic->id)}}">
-      <div class="card d-flex justify-content-center">
+      <div class="card p-3 d-flex justify-content-center">
         <h3>{{$comic->title}}</h3>
         <figure><img src="{{$comic->thumb}}" width="150" alt="{{$comic->title}}"></figure>
         <h5>{{$comic->series}}</h5>
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+  <div id="comics-list" class="row text-center justify-content-center">
+    <div class="card p-3 d-flex justify-content-center">
+      <h3>{{$comic->title}}</h3>
+      <figure><img src="{{$comic->thumb}}" width="250" alt="{{$comic->title}}"></figure>
+      <h5>{{$comic->series}}</h5>
+      <h5>{{$comic->sale_date}}</h5>
+      <p>{{$comic->description}}</p>
+      <div><strong>Prezzo: €{{$comic->price}}</strong></div>
+    </div>
+  </div>
+</div>
+
+@endsection
