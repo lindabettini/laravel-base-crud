072e49652e3df7f6b329e22d46e7d5d358fe64ba
lindabettini <linda.bettini@hotmail.it>
2022-03-22 23:47:42 +0100

    stye btn

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 
-<div class="container">
+<div id="card-details" class="container">
   <div id="comics-list" class="row text-center justify-content-center">
-    <div class="card p-3 d-flex justify-content-center">
+    <div class="card p-3 m-3 d-flex justify-content-center">
       <h3>{{$comic->title}}</h3>
       <figure><img src="{{$comic->thumb}}" width="250" alt="{{$comic->title}}"></figure>
       <h5>{{$comic->series}}</h5>
       <h5>{{$comic->sale_date}}</h5>
       <p>{{$comic->description}}</p>
       <div><strong>Prezzo: €{{$comic->price}}</strong></div>
-      <a class="btn btn-secondary" href="{{route('comics.index')}}">Indietro</a>
+      <div><a class="btn btn-secondary m-3" href="{{route('comics.index')}}">Indietro</a></div>
     </div>
   </div>
 </div>
