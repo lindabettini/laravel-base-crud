015c98a21029ebf639da76278e20acffcae86e1c
lindabettini <linda.bettini@hotmail.it>
2022-03-22 18:54:23 +0100

    add cards

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        $comics = [];
+        $comics = Comic::all();
         return view('comics.index', compact('comics'));
     }
 
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.main')
 
-@section ('content')
+@section('content')
 <h1>Comics</h1>
-<div class="card-body">
+<div class="row text-center justify-content-center">
+  @foreach ($comics as $comic)
+  <dic class="col-5 p-3 m-3">
+    <div class="card d-flex justify-content-center">
+      <h3>{{$comic->title}}</h3>
+      <figure><img src="{{$comic->thumb}}" width="150" alt="{{$comic->title}}"></figure>
+      <h5>{{$comic->series}}</h5>
+      <h5>{{$comic->sale_date}}</h5>
+      <p>{{$comic->description}}</p>
+      <div><strong>Prezzo: €{{$comic->price}}</strong></div>
+    </div>
+  </dic>
+  @endforeach
 
 </div>
 @endsection
