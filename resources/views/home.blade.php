8ec27f54fa65ece44fb1d46bf269ea5c373153c5
lindabettini <linda.bettini@hotmail.it>
2022-03-23 17:24:31 +0100

    add link to index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="text-center">
   <h1>Benvenuto</h1>
-  <h3>Scopri tutti i nostri Comics!</h3>
+  <h3><a href="{{route('comics.index')}}">Scopri tutti i nostri Comics!</a></h3>
   <a class="btn btn-primary m-3" href="{{route('comics.create')}}">Aggiungi un Comic</a>
   @endsection
